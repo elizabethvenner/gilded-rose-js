3e6ee74b44f94b57ff3e4499b8d631388336e3b3
elizabethvenner <elizabethvenner@googlemail.com>
2016-12-01 11:40:31 +0000

    create special_items array and check if an item is included in it

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -7,16 +7,20 @@ describe("Gilded Rose", function() {
   });
 
   describe('update_quality', function() {
+
+    items = [new Item('item', 2, 2), new Item('Aged Brie', 2, 2)];
+    update_quality();
+
     it('decreases sell_in by 1', function() {
-      items = [new Item('item', 2, 2)];
-      update_quality();
       expect(items[0].sell_in).toEqual(1);
     });
 
     it('decreases quality by 1', function() {
-      items = [new Item('item', 2, 2)];
-      update_quality();
       expect(items[0].quality).toEqual(1);
+    });
+
+    it('does not decrease quality of aged brie', function() {
+      expect(items[1].quality).toEqual(2);
     });
   });
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -5,10 +5,12 @@ function Item(name, sell_in, quality) {
 }
 
 var items = []
+var special_items = ['Aged Brie']
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    regular_item_update(items[0]);
+    if (!special_items.includes(items[i].name))
+    regular_item_update(items[i]);
   }
 }
 
