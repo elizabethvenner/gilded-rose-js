19d1b58090a5179f98a29b2347ea841cfe0d0cc5
elizabethvenner <elizabethvenner@googlemail.com>
2016-12-01 11:18:27 +0000

    delete most of code and write function to decrease sell_in and quality for regular items

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,22 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
+  xit("should foo", function() {
     items = [ new Item("foo", 0, 0) ];
     update_quality();
     expect(items[0].name).toEqual("fixme");
   });
 
+  describe('update_quality', function() {
+    it('decreases sell_in by 1', function() {
+      items = [new Item('item', 2, 2)];
+      update_quality();
+      expect(items[0].sell_in).toEqual(1);
+    });
+
+    it('decreases quality by 1', function() {
+      items = [new Item('item', 2, 2)];
+      update_quality();
+      expect(items[0].quality).toEqual(1);
+    });
+  });
 });
