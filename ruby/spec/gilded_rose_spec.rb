28d7e0d109636cbae194ec0f890a9e0b76bc6345
Elizabeth <elizabethvenner@googlemail.com>
2017-01-12 18:01:22 +0000

    write test to check that instances of GildedRose send update_quality method to items in items array

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,14 +1,14 @@
 require 'gilded_rose'
-require 'item'
 
 describe GildedRose do
+  let(:item) {double :item}
+  subject(:inn) {described_class.new([item])}
 
-  # describe "#update_quality" do
-  #   it "does not change the name" do
-  #     items = [Item.new("foo", 0, 0)]
-  #     GildedRose.new(items).update_quality()
-  #     expect(items[0].name).to eq("foo")
-  #   end
-  # end
+  describe "#update_quality" do
+    it "sends message to item" do
+      expect(item).to receive(:update_quality)
+      inn.update_quality
+    end
+  end
 
 end
